932358deac319fca10f729531cd23e010b8c8f66
jsw0227 <zhalrtjddn01@gmail.com>
2025-11-26 12:25:05 +0900

    autowire 만들기

--- Backend/spring/spring1126/src/main/java/ex1/Ex1_AutoWired.java
+++ Backend/spring/spring1126/src/main/java/ex1/Ex1_AutoWired.java
@@ -1,7 +1,14 @@
 package ex1;
 //AutoWired : 자동 빈 묶기 , 의존성을 자동으로 연결 *****
 //byName, byType 구분하기
+//byName : Properties 와 id가 같을 경우 , 현재 페이지의 private String msg 와 ex1_auto 페이지의 id값
+//byType : 자료형이 같은 경우
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Ex1_AutoWired {
+	@Autowired 
+	@Qualifier("ictMsg2")
 	private String msg;
 	
 	public String getMessage() {
@@ -13,9 +20,11 @@ public class Ex1_AutoWired {
 		return sb.toString();
 	}
 
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
+//	@Autowired
+//	public void setMsg(String msg) {
+//		this.msg = msg;
+//	}
+	
 	
 	
 	
---
+++ Backend/spring/spring1126/src/main/java/ex1/Ex2_Resource.java
@@ -0,0 +1,18 @@
+package ex1;
+
+public class Ex2_Resource {
+
+	
+	private Ex2_MyResource res;
+
+	public Ex2_MyResource getRes() {
+		return res;
+	}
+
+	public void setRes(Ex2_MyResource res) {
+		this.res = res;
+	}
+	
+	
+	
+}
